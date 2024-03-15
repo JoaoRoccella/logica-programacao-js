9935070f68567e23ea91d0f6437ca271f8addd26
Joao Roccella <roccella@gmail.com>
2024-03-15 13:45:08 +0000

    Enunciados dos desafios

--- operadores-aritmeticos.js
+++ operadores-aritmeticos.js
@@ -1,5 +1,5 @@
 const numero1 = 40;
-const numero2 = 20;
+const numero2 = 2;
 
 // copiar linhas: alt + shift + ⬇
 console.log(`Os números das operações são ${numero1} e ${numero2}`);
@@ -8,3 +8,4 @@ console.log(`A subtração dos números é ${numero1 - numero2}`);
 console.log(`A multiplicação dos números é ${numero1 * numero2}`);
 console.log(`A divisão dos números é ${numero1 / numero2}`);
 console.log(`O resto da divisão dos números é ${numero1 % numero2}`);
+console.log(`O numero1 elevado à potência do numero2 é ${numero1 ** numero2}`);
