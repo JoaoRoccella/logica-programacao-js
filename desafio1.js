944e17d6ceeb2195287552a3aeafa0d03bab8f8e
guilherme amorim <cezarguilherme03@gmail.com>
2024-03-15 17:00:30 -0300

    adicionando arquivo sobre operadores lógicos

--- desafio1.js
+++ desafio1.js
@@ -1,10 +1,29 @@
-// Escreva um código em JavaScript que resolva o problema descrito no livro Lógica de Programação I, p. , em que temos que obter as 3 notas de um aluno, calcular a média e exibir se o aluno foi aprovado, reprovado ou se ficou para exame. Não é necessário ler as notas, você pode defini-las com variáveis.
+// Escreva um código em JavaScript que resolva o problema descrito no livro Lógica de Programação I, p. , em que temos que obter as 3 notas de um aluno, calcular a média e exibir se o aluno foi aprovado, reprovado ou se ficou para recuperação. Não é necessário ler as notas, você pode defini-las com variáveis.
 // Você precisará utilizar uma estrutura de decisão composta, como a seguir:
 
-if (condicao1) {
-    // código da condição 1
-} else if (condicao2) {
-    // código da condição 2
+//correção
+//declaração - identificador - tipo (valor)
+const nomeAluno = "Ricardo";
+
+//entrada de dados
+const nota01 = 3;
+const nota02 = 4;
+const nota03 = 10;
+
+//processamento de dados - conta
+const mediaFinal = (nota01 + nota02 + nota03) / 3;
+    
+if (mediaFinal >= 7) {
+  //saída de dados
+  console.log(
+    `O aluno: ${nomeAluno}. Foi aprovado com média final de: ${mediaFinal.toFixed(2)}`
+  );
+} else if (mediaFinal >= 5 && mediaFinal <= 6) {
+  //saída de dados
+  console.log(
+    `O aluno: ${nomeAluno}. Esta de recuperação com média final de: ${mediaFinal.toFixed(2)}`
+  );
 } else {
-    // código se nenhuma das condições for verdadeira
+  //saída de dados
+  console.log(`Aluno reprovado`);
 }
