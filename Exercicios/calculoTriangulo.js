c0d5091a1f6d2e65a21fae65f04c9833ae2e8d56
JoaoRoccella <roccella@gmail.com>
2024-03-21 16:29:14 -0300

    Resolução calculoTriangulo

--- Exercicios/calculoTriangulo.js
+++ Exercicios/calculoTriangulo.js
@@ -17,3 +17,30 @@
 //saida de dados
 // = informar se o triangulo é equilátero, escaleno ou isócele
 // = motrar a saída do programa na console
+function verificarTriangulo() {
+
+    const
+        lado1 = Number(window.prompt('Valor do lado 1:')),
+        lado2 = Number(window.prompt('Valor do lado 2:')),
+        lado3 = Number(window.prompt('Valor do lado 3:'));
+
+    if (lado1 < (lado2 + lado3) && lado2 < (lado1 + lado3) && lado3 < (lado1 + lado2)) {
+
+        if (lado1 === lado2 && lado2 === lado3) {
+            console.log('O triângulo é Equilátero');
+
+            // Teste do Isósceles: (lado1 === lado2 && lado2 !== lado3 || lado2 === lado3 && lado3 !== lado1 || lado1 === lado3 && lado3 !== lado2) 
+
+            // simplificação da lógica ⬇
+
+        } else if (lado1 === lado2 || lado2 === lado3 || lado3 === lado1) {
+            console.log('O triângulo é Isósceles');
+        } else {
+            console.log('O triângulo é Escaleno');
+        }
+
+    } else {
+        console.log('A forma não é um triângulo');
+    }
+
+}
--- Exercicios/jogoFutebol.js
+++ Exercicios/jogoFutebol.js
@@ -5,10 +5,18 @@
 // = informar os dois times de futebol
 // = informar o placar do jogo
 
-
 //processamento
 // = realizar a validação através das estruturas condicionais
 // = verificar se os times ganharam, perderam ou empataram
 
 //saida
 // = informar o resultado do jogo de futebol
+const placar = '3x2';
+const [golsTime1, golsTime2] = placar.split('x');
+
+console.log(`COR - ${golsTime1} x ${golsTime2} - PAL`);
+
+// não tem a ver com o futebol, é só outro exemplo
+const [nome, sobrenome] =  'João Roccella'.split(' ');
+
+console.log(`Nome: ${nome} - Sobrenome: ${sobrenome}`);
