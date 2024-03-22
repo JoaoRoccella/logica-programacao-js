50cd49cd340e127cf2d3441e9aaf70166a6944c1
guilherme amorim <cezarguilherme03@gmail.com>
2024-03-22 17:04:35 -0300

    falta 2 exercicios

--- Exercicios/nomesSortidos.js
+++ Exercicios/nomesSortidos.js
@@ -2,4 +2,3 @@
 // lidos em um vetor. Após isto, o algoritmo deve permitir a leitura de mais 1 nome qualquer de
 // pessoa (para efetuar uma busca) e depois escrever a mensagem ACHEI, se o nome estiver
 // entre os 10 nomes lidos anteriormente (guardados no vetor), ou NÃO ACHEI caso contrário.
-
